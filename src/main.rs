d7b6c1a90656da4c3bc395f9a952f66885819ad6
daenikon <dkondratiuk06@gmail.com>
2025-07-23 16:02:37 +0200

    feat: add basic CLI structure with start, status, and commit commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "git-timer")]
+#[command(about = "A coding time tracker", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Starts timer
+    #[command()]
+    Start {
+        /// Timer's name
+        timer_name: String,
+    },
+    /// TODO
+    #[command()]
+    Status,
+    /// Commit and add timer data
+    #[command(arg_required_else_help = true)]
+    Commit {
+        /// Commit message
+        message: String,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Start { timer_name } => {
+            println!("Started timer {timer_name}");
+        },
+        Commands::Status => {
+            println!("Status info");
+        },
+        Commands::Commit { message } => {
+            println!("Ran command: git commit -m \"{message}\"");
+        },
+    }
 }
